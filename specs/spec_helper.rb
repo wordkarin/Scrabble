aba50082887e18e921462bb2a1784e4d1c0ffb67
Natasha Ewing <natasharai@me.com>
2016-08-31 13:54:16 -0700

    strip punctuation working now

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -1,4 +1,8 @@
-# KNK: I've just copied the spec_helper.rb file from the in-class TDD demo. 
+# KNK: This is from the simplecov documentation
+require 'simplecov'
+SimpleCov.start
+
+# KNK: I've just copied the spec_helper.rb file from the in-class TDD demo.
 require 'minitest'
 require 'minitest/spec'
 require "minitest/autorun"
