3c1bbcd3e588b0bcda1b258b63cbd8888ee5ed29
Karin Kubischta <karin.kubischta@gmail.com>
2016-09-01 14:57:54 -0700

    updated tile_spec and tile_bag with comments

--- specs/tile_spec.rb
+++ specs/tile_spec.rb
@@ -1,36 +1,35 @@
 require_relative 'spec_helper'
-require_relative 'scoring.rb'
-require_relative 'tile_bag'
+require_relative '../tile_bag'
 
-
-
-with a __minimum of 5 specs__. It should have the following class and instance methods:
-`#initialize` Called when you use `TileBag.new`, sets up an instance with a collection of default tiles
-`#draw_tiles(num)` returns `num` number of random tiles, removes the tiles from the default set.
-`#tiles_remaining` returns the number of tiles remaining in the bag
-
+# with a __minimum of 5 specs__. It should have the following class and instance methods:
+# `#initialize` Called when you use `TileBag.new`, sets up an instance with a collection of default tiles
+# `#draw_tiles(num)` returns `num` number of random tiles, removes the tiles from the default set.
+# `#tiles_remaining` returns the number of tiles remaining in the bag
+#
 module Scrabble
   describe TileBag do
     let(:tile){TileBag.new}
     describe "#initialize" do
-
       it "should instantiate collection of tiles" do
-      #strategy: each instance of TileBag sets up new collection of tiles
-      # using attribute reader, @collection = collection, returns array of tiles. self.collection method holds each instance of collection mutating as tiles are drawn.
-      tile.collection.must_return_array(collection)
+        # strategy: each instance of TileBag sets up new collection of tiles
+        # using attribute reader, @collection = collection, returns array of tiles. self.collection method holds each instance of collection mutating as tiles are drawn.
+        # tile.collection.must_return_array(collection)
       end
+    end
 
-      describe "draw_tiles" do
+    describe "#draw_tiles(num)" do
       it "should return the number of default tiles" do
         #num: returns the value of the
         # Strategy: create method with parameter 'num' that returns 'num' default tiles and values by implementing 'tile.sample(num)' since each instance creates its own array draw_tiles method should use mutate that mutates using ruby pop method each time draw_tiles is called. this should return equal number of default tiles and the collection array should then reflect self - num of tiles drawn.
-        tile.draw_tiles.must_equal_num(num)
+        tile.draw_tiles(num).must_equal(num)
       end
     end
 
-    describe "" do
+    describe "#tiles_remaining" do
       it "should return an array of the words played by the player" do
-        # #plays: returns collection of array at any time with players tiles remaining
-        tile.collection.must_return_updated(collection)
+        #plays: returns collection of array at any time with players tiles remaining
+        # tile.collection.must_return_updated(collection)
       end
     end
+  end
+end
--- tile_bag.rb
+++ tile_bag.rb
@@ -1,38 +1,40 @@
-require_relative 'scoring.rb' 
-
-class TileBag < Scrabble
-  attr_reader(tile)
+require_relative 'scoring.rb'
+module Scrabble
+  class TileBag
+  attr_reader :tile
+  end 
+end
 #### `TileBag`
-Create a `Scrabble::TileBag` class with a __minimum of 5 specs__. It should have the following class and instance methods:
-
-- `#initialize` Called when you use `TileBag.new`, sets up an instance with a collection of default tiles
-- `#draw_tiles(num)` returns `num` number of random tiles, removes the tiles from the default set.
-- `#tiles_remaining` returns the number of tiles remaining in the bag
-
-##### Initial Distribution of Letters
-| Letter : Qty. |
-|:------:|:-----:|
-| A : 9  | N : 6 |
-| B : 2  | O : 8 |
-| C : 2  | P : 2 |
-| D : 4  | Q : 1 |
-| E : 12 | R : 6 |
-| F : 2  | S : 4 |
-| G : 3  | T : 6 |
-| H : 2  | U : 4 |
-| I : 9  | V : 2 |
-| J : 1  | W : 2 |
-| K : 1  | X : 1 |
-| L : 4  | Y : 2 |
-| M : 2  | Z : 1 |
-
-#### Modifications to `Player`
-Create specs for (__minimum 2__) and add to the `Player` class the following instance methods:
-
-- `#tiles` a collection of letters that the player can play (max 7)
-- `#draw_tiles(tile_bag)` fills tiles array until it has 7 letters from the given tile bag
-
-### Optional Enhancements
-- Create a `Scrabble::Dictionary` class that includes a method (class or instance) for searching a list of words to determine if a given word is valid (__minimum of 5 specs__).
-- Create a `Scrabble::Board` class (__minimum of 15 specs__) that has a matrix (array of arrays) of tile places. Check if a word can be played on a given tile place in a certain direction (up/down or left/right).
--->
+# Create a `Scrabble::TileBag` class with a __minimum of 5 specs__. It should have the following class and instance methods:
+#
+# - `#initialize` Called when you use `TileBag.new`, sets up an instance with a collection of default tiles
+# - `#draw_tiles(num)` returns `num` number of random tiles, removes the tiles from the default set.
+# - `#tiles_remaining` returns the number of tiles remaining in the bag
+#
+# ##### Initial Distribution of Letters
+# | Letter : Qty. |
+# |:------:|:-----:|
+# | A : 9  | N : 6 |
+# | B : 2  | O : 8 |
+# | C : 2  | P : 2 |
+# | D : 4  | Q : 1 |
+# | E : 12 | R : 6 |
+# | F : 2  | S : 4 |
+# | G : 3  | T : 6 |
+# | H : 2  | U : 4 |
+# | I : 9  | V : 2 |
+# | J : 1  | W : 2 |
+# | K : 1  | X : 1 |
+# | L : 4  | Y : 2 |
+# | M : 2  | Z : 1 |
+#
+# #### Modifications to `Player`
+# Create specs for (__minimum 2__) and add to the `Player` class the following instance methods:
+#
+# - `#tiles` a collection of letters that the player can play (max 7)
+# - `#draw_tiles(tile_bag)` fills tiles array until it has 7 letters from the given tile bag
+#
+# ### Optional Enhancements
+# - Create a `Scrabble::Dictionary` class that includes a method (class or instance) for searching a list of words to determine if a given word is valid (__minimum of 5 specs__).
+# - Create a `Scrabble::Board` class (__minimum of 15 specs__) that has a matrix (array of arrays) of tile places. Check if a word can be played on a given tile place in a certain direction (up/down or left/right).
+# -->
